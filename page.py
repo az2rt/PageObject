25ac8b99cc39340f1da4b3e456d00af2df5fcad4
KUDRYASHOV ILYA <i.s.kudrjashov@gmail.com>
2019-08-26 18:00:30 +0300

    second

--- page.py
+++ page.py
@@ -1,23 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 class BasePage(object):
 
     def __init__(self):
-        self.driver=webdriver.chrome('./bin/chromedriver')
+        self.driver=webdriver.Chrome('./bin/chromedriver')
 
     def open(self, url):
         return self.driver.get(url)
 
+    def close(self):
+        self.driver.close()
 
 class SearchPage(BasePage):
 
     def set_search_phrase(self, search_phrase):
-        field = self.driver.find_element_by_class_name('vdLsw gsfi')
-        field.input(search_phrase)
-        field.send_keys(Keys.RETURN)
+        try:
+            field = self.driver.find_element_by_xpath('//input[contains(@class, \'search__input\')]')
+            field.send_keys(search_phrase)
+            field.send_keys(Keys.RETURN)
+        except ( NoSuchElementException, AttributeError):
+            import ipdb; ipdb.set_trace()
 
     def get_nav_bar(self):
-        return self.driver.find_element_by_class_name('navcnt')
+        return self.driver.find_element_by_xpath('//a[contains(@class, \'PageNavigator\')]')
 
--- tests.py
+++ tests.py
@@ -1,12 +1,20 @@
 import unittest
 from page import SearchPage
 
+
 class TestCases(unittest.TestCase):
 
     def setUp(self):
         self.page = SearchPage()
 
+    def tearDown(self):
+        self.page.close()
+
     def test_check_search(self):
-        self.page.open('http://google.com')
+        self.page.open('http://mail.ru')
         self.page.set_search_phrase('Qwerty')
         assert self.page.get_nav_bar()
+
+
+if __name__ == '__main__':
+    unittest.main()
